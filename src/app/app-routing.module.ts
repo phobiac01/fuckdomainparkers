106d19fd478d1d543ccecae68d8bb185030a2d76
phobiac01 <phobiac01@gmail.com>
2020-10-17 16:56:27 -0700

    Added a Flexbox footer, added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'feet', component: FooterComponent },
+  { path: '**', component: HomepageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
